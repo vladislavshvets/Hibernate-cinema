c19b9f93dc39627405485327570845c76e5c16cd
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-11-06 09:40:07 +0200

    fixed fix checkstyle ^^"

--- src/main/java/com/dev/cinema/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/AuthenticationServiceImpl.java
@@ -9,27 +9,29 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthenticationServiceImpl implements AuthenticationService {
+public class AuthenticationServiceImpl
+        implements AuthenticationService {
     private final UserService userService;
     private final ShoppingCartService shoppingCartService;
     private final RoleService roleService;
 
     public AuthenticationServiceImpl(UserService userService,
-        ShoppingCartService shoppingCartService,
-        RoleService roleService) {
-            this.userService = userService;
-            this.shoppingCartService = shoppingCartService;
-            this.roleService = roleService;
-        }
+                                     ShoppingCartService shoppingCartService,
+                                     RoleService roleService) {
+        this.userService = userService;
+        this.shoppingCartService = shoppingCartService;
+        this.roleService = roleService;
+    }
 
-        @Override
-        public User register(String email, String password) {
-            User user = new User();
-            user.setEmail(email);
-            user.setPassword(password);
-            user.setRoles(Set.of(roleService.getRoleByName("USER")));
-            userService.add(user);
-            shoppingCartService.registerNewShCart(user);
-            return user;
-        }
+    @Override
+    public User register(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRoles(Set.of(roleService
+                .getRoleByName("USER")));
+        userService.add(user);
+        shoppingCartService.registerNewShCart(user);
+        return user;
+    }
 }
